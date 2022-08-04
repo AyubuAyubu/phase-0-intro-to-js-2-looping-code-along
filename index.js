59483a51f2916b200f32ddfc661f40a80c074b6d
AyubuAyubu <ayubu6143@gmail.com>
2022-08-04 13:44:24 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function countDown() {
+    for (let countDown = 0; countDown < 11; countDown++) {
+      console.log(countDown);
+    }
+  }
+  const newArr = [];
+  function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+      newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      debugger;
+    }
+    return newArr;
+  }
